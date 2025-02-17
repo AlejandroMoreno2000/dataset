3889d0c9251f10f5d5c70893d9572bc0e5bcdecb
AlejandroMoreno2000 <80093537+AlejandroMoreno2000@users.noreply.github.com>
2025-02-17 18:04:17 +0100

    goodware dataset

---
+++ goodware/proyecto-ejecucion/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    printf("Hola a todos!\n");
+    bool esTrue = true;
+    if(esTrue){
+        printf("Esto es un ejemplo\n");
+    }
+    for(int i = 0; i < 5; i++){
+        printf("Sirve como fichero auxiliar\n");
+    }
+    printf("Me quedo sin imaginacion por culpa de TikTok\n");
+    return 0;
+}
